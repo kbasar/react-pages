0bc2e563e6661accd8cc0e1b8cbe580319b914f5
Khairul Basar <khairul.basar@external.merckgroup.com>
2023-08-12 21:49:20 -0500

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React - I made it
         </a>
       </header>
     </div>
